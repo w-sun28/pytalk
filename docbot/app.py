3eb3e565997cdbf61c6ab485db7a01c23f77defd
Sun <ws0163@lxcz7ht52-cse.unt.ad.unt.edu>
2020-06-10 23:47:22 -0500

    added file uploading

--- docbot/app.py
+++ docbot/app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, request
+import os
+from flask import Flask, render_template, request, flash, redirect, url_for, send_from_directory
+from werkzeug.utils import secure_filename
 
 from docbot import *
 # production server
@@ -29,13 +31,22 @@ bots = {
   "wolfram"	: None
 }
 
+UPLOAD_FOLDER = '/home/UNT/ws0163/Documents/pytalk/examples'
+ALLOWED_EXTENSIONS = {'pdf'}
+
 def activate_bot(name) :
   if not bots[name]:
      bots[name] = Bot("../examples/"+name+".txt")
   return bots[name]
 
+# returns whether the file uploaded has correct filetype
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 # the Flask-based Web app
 app = Flask(__name__,static_url_path='/static')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route("/")
 def home():
@@ -70,6 +81,39 @@ def get_bot_response():
           return bot.ask(userText)
         except:
           return "Sorry, I have no answer to that."
+
+# receives uploaded file
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            print(filename)
+            bot = Bot("../examples/" + filename)
+            botname = filename[:-4]
+            bots[botname] = bot
+            print(bots)
+            return redirect(request.url)
+            # return redirect(url_for('uploaded_file',filename=filename))
+
+    return 0
+
+# method used once user is redirected from upload_file()
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
 
 if __name__ == "__main__":
   '''
